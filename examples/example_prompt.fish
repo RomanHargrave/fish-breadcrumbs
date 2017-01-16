f206e7666ff1c9ad1af0cbd22e3e758913bbd762
Roman Hargrave <roman@hargrave.info>
2017-01-16 16:02:06 -0600

    Add example

---
+++ examples/example_prompt.fish
@@ -0,0 +1,35 @@
+function _example_fish_prompt
+    set _last_status $status
+    set -l _sep_colour 444
+
+	if not set -q -g __prompt_sorin_functions_defined
+		set -g __prompt_sorin_functions_defined
+	end
+
+    switch $_last_status
+        case 0
+            set_color green
+        case '*'
+            set_color red
+    end
+
+    printf 'E%d'(set_color $_sep_colour)' : ' $_last_status
+
+    ######################
+    ## Fish Breadcrumbs ##
+    ######################
+    __fish_breadcrumbs_print (set_color $_sep_colour)' : '
+
+    # SSH
+	if set -q SSH_TTY
+        printf (set_color red)(whoami)(set_color white)'@'(set_color yellow)(hostname)' '
+    end
+
+    # root warning
+    if [ $USER = 'root' ]
+        printf (set_color red)"#"
+    end
+
+    # Main
+	echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+end
